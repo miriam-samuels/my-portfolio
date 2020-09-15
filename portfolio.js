c7c898022941b556b9fef015aa2ff757a0d838f0
miriam-samuels <48389614+miriam-samuels@users.noreply.github.com>
2020-09-15 15:13:54 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -5,12 +5,33 @@ function move() {
     var width = 10;
     var id = setInterval(frame, 20);
     function frame() {
-            if (width > 90) {
+            if (width > 89) {
                 clearInterval(id)
             }else {
                 width++;
                 elem[0].style.width = width + 'px';
                label[0].innerHTML = width * 1 + '%';
+            }
+           if (width > 84) {
+                clearInterval(id)
+            }else {
+                width++;
+                elem[1].style.width = width + 'px';
+               label[1].innerHTML = width * 1 + '%';
+            }
+           if (width > 64) {
+                clearInterval(id)
+            }else {
+                width++;
+                elem[2].style.width = width + 'px';
+               label[2].innerHTML = width * 1 + '%';
+           }
+           if (width > 49) {
+                clearInterval(id)
+            }else {
+                width++;
+                elem[3].style.width = width + 'px';
+               label[3].innerHTML = width * 1 + '%';
             }
     }
   }

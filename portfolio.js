2f728569ffd90b6cb1c62cff2a5535481a6524b0
miriam-samuels <48389614+miriam-samuels@users.noreply.github.com>
2020-09-15 15:30:13 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -4,6 +4,9 @@ function move() {
     var label = Array.from( document.getElementsByClassName("label"))
     var width = 10;
     var id = setInterval(frame, 20);
+    var id2 = setInterval(frame2, 20);
+    var id3 = setInterval(frame3, 20);
+    var id4 = setInterval(frame4, 20);
     function frame() {
             if (width > 90) {
                 clearInterval(id)
@@ -12,22 +15,29 @@ function move() {
                 elem[0].style.width = width + 'px';
                label[0].innerHTML = width + '%';
             }
+         }
+    function frame2() {
            if (width > 85) {
-                clearInterval(id)
+                clearInterval(id2)
             }else {
                 width++;
                 elem[1].style.width = width + 'px';
                label[1].innerHTML = width + '%';
             }
+          }
+     function frame3() {
            if (width > 50) {
-                clearInterval(id)
+                clearInterval(id3)
             }else {
                 width++;
                 elem[2].style.width = width + 'px';
                label[2].innerHTML = width + '%';
            }
+           }
+     
+       function frame4() {
            if (width > 65) {
-                clearInterval(id)
+                clearInterval(id4)
             }else {
                 width++;
                 elem[3].style.width = width + 'px';

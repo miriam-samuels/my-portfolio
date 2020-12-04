5ebb1041a290a70f75b338117560818a5dea2215
miriam-samuels <medomiriam@gmail.com>
2020-12-04 14:08:40 +0100

    drop-down

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function show(){
+function show() {
     const nav_item = document.querySelector('header #nav-item');
-     if(nav_item.style.display === "none") {  
-        nav_item.style.display = "block";  
-     } else {  
-        nav_item.style.display = "none";  
-     }  
-    }
+    nav_item.style.display = "block";
+}
+function unshow() {
+    const nav_item = document.querySelector('header #nav-item');
+    nav_item.style.display = "none";
+}

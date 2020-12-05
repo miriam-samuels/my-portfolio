74cfbc50aa6e3a49eca76bf5853a7cd8c14211a7
miriam-samuels <medomiriam@gmail.com>
2020-12-05 09:51:01 +0100

    about

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function show() {
-    const nav_item = document.querySelector('header #nav-item');
+    const nav_item = document.getElementById('nav-item');
     nav_item.style.display = "block";
 }
 function unshow() {
-    const nav_item = document.querySelector('header #nav-item');
+    const nav_item = document.getElementById('nav-item');
     nav_item.style.display = "none";
 }

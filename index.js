8829838649eba853b053699484a831741579fc92
miriam-samuels <medomiriam@gmail.com>
2020-12-05 23:08:37 +0100

    email

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ function unshow() {
     const nav_item = document.getElementById('nav-item');
     nav_item.style.display = "none";
 }
+function sendEmail() {
+    const sender_mail = document.getElementById('sender-mail');
+    const email_subject = document.getElementById('email-subject');
+    const sender_message = document.getElementById('sender-message');
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username : sender_mail.value,
+        Password : "Medo@1994",
+        To : 'medomiriam@gmail.com',
+        From : sender_mail.value,
+        Subject : email_subject.value,
+        Body : sender_message.value,
+        }).then(
+            message => alert("mail sent successfully")
+        ); 
+}
